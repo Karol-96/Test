b5e2c4917a018b5359c8155f80c01b44f105dba3
Karol-96 <70049752+Karol-96@users.noreply.github.com>
2024-07-31 23:26:41 +0545

    Create app.py

---
+++ app.py
@@ -0,0 +1,90 @@
+from flask import Flask, request, jsonify
+import pyodbc
+import os
+from werkzeug.utils import secure_filename
+import pandas as pd
+from io import StringIO
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = '/path/to/upload'
+
+conn_str = (
+    r'DRIVER={ODBC Driver 17 for SQL Server};'
+    r'SERVER=48.216.216.71;'
+    r'DATABASE=HTTEST;'
+    r'UID=karolbhandari;'
+    r'PWD=karolbhandari@2024'
+)
+
+def get_db_connection():
+    return pyodbc.connect(conn_str)
+
+@app.route('/', methods=['GET'])
+def home():
+    return "Welcome to the API!"
+
+@app.route('/call_procedure', methods=['POST'])
+def call_procedure():
+    data = request.json
+    df = pd.DataFrame(data)
+    try:
+        # Establish connection
+        conn = get_db_connection()
+        cursor = conn.cursor()
+
+        # Create a temporary table to hold the TVP data
+        cursor.execute("""
+            IF OBJECT_ID('tempdb..#TempNewMemberships') IS NOT NULL
+            DROP TABLE #TempNewMemberships;
+                                
+            CREATE TABLE #TempNewMemberships 
+            (
+                MembershipID INT,
+                MemberName NVARCHAR(100),
+                BirthDate DATE
+            )
+                        
+            print 'Temp Table Created'""")
+        conn.commit()
+                # Insert DataFrame data into the temporary table
+        for index, row in df.iterrows():
+            cursor.execute("""
+                INSERT INTO #TempNewMemberships (MembershipID, MemberName, BirthDate)
+                VALUES (?, ?, ?)
+            """, row['MembershipID'], row['MemberName'], row['BirthDate'])
+                # Commit the transaction
+        conn.commit()
+        print('Data inserted into Temp Table')
+        cursor.execute("""                                
+            DECLARE @Memberships MyNewMembershipType
+
+            INSERT INTO @Memberships (MembershipID, MemberName, BirthDate)
+            SELECT * FROM #TempNewMemberships
+
+            DECLARE @MembershipsOutbound MyNewMembershipTypeOutbound
+
+            INSERT INTO @MembershipsOutbound 
+            exec [dbo].[NewAgeCalculator] @Memberships
+
+            drop table if exists #output
+            select * into #output from @MembershipsOutbound
+         """)
+        cursor.execute("""select * from #output""")
+        results = cursor.fetchall()  
+        print("Results from TVP:", results)
+        conn.commit() 
+        
+        result_list = [dict(zip([column[0] for column in cursor.description], row)) for row in results]
+
+        # Close the cursor and connection
+        cursor.close()
+        conn.close()
+
+        return jsonify(result_list), 200
+    
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True,host="0.0.0.0",port=8080)
